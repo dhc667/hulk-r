d8dfb790c4ed2eb1a8ccdde6e23ada5ca5ba9d05
CfM47 <jossuearteche000@gmail.com>
2025-04-20 13:46:45 -0400

    test: 💍 Added fibonacci test
    Added misc.rs to the tests folder. We are turing complete now

--- generator/src/test/mod.rs
+++ generator/src/test/mod.rs
@@ -4,19 +4,18 @@ use crate::GeneratorVisitor;
 
 pub mod lli_interface;
 
-pub mod operators;
-pub mod let_in;
-pub mod dassignment;
 pub mod block;
+pub mod dassignment;
 pub mod if_else;
+pub mod let_in;
+pub mod misc;
+pub mod operators;
 pub mod while_loop;
 
-fn generate_code(hulk: &str) -> String{
+fn generate_code(hulk: &str) -> String {
     let p = ProgramParser::new();
     let mut ast = p.parse(hulk).unwrap();
     let mut visitor = GeneratorVisitor::new();
     let code = ast.accept(&mut visitor);
     return code.preamble;
 }
-
-
