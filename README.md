b3ed63b86f27ffa638b1f11d96b8542482086e29
dhc667 <dariohcvg@gmail.com>
2025-04-24 19:00:11 -0400

    feat(runner): add runner binary and Makefile for showcase

---
+++ README.md
@@ -0,0 +1,7 @@
+# Running the project
+
+1. Execute `make`
+2. Write your valid hulk code in a file named `main.hulk` in the root of the project
+3. Execute `make compile` to generate a `main.ll` file or, if you have `lli` installed, run `make execute` to compile and execute
+
+
---
+++ runner/src/runner.rs
@@ -0,0 +1,35 @@
+use generator::GeneratorVisitor;
+use parser::{ProgramParser, Visitable};
+use parser::visitors::implementations::semantic_visitor::SemanticVisitor;
+
+
+fn write_output(target_file: &str, content: &str) -> Result<(), std::io::Error> {
+    std::fs::write(target_file, content)?;
+    Ok(())
+}
+
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let file = "main.hulk";
+    let output_file = "main.ll";
+
+    let content = std::fs::read_to_string(file)?;
+
+    let p = ProgramParser::new();
+    let mut ast = p.parse(&content).map_err(|e| format!("Parse error: {}", e))?;
+    let mut semantic_visitor = SemanticVisitor::new();
+    
+    ast.accept(&mut semantic_visitor);
+    if !semantic_visitor.errors.is_empty() {
+        for error in semantic_visitor.errors {
+            println!("Error: {}", error);
+        }
+        return Err("Semantic errors found".into());
+    }
+
+    let mut generator_visitor = GeneratorVisitor::new();
+
+    let generated_code = ast.accept(&mut generator_visitor).preamble;
+
+    write_output(output_file, &generated_code)?;
+    Ok(())
+}
