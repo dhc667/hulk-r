8c2204ba1393ee8d9dc0ce1d0161892c65ae6e90
francisco-prestamo <prestamo2003@icloud.com>
2025-06-05 17:58:52 -0400

    feat: Enhance code generation for data member access and function definitions

--- generator/src/test/mod.rs
+++ generator/src/test/mod.rs
@@ -23,6 +23,14 @@ fn generate_code(hulk: &str) -> String {
     let mut semantic_analyzer = SemanticAnalyzer::new();
     let analysis_result = semantic_analyzer.analyze_program_ast(&mut ast);
     
+    if let Err(errors) = analysis_result {
+        for error in errors {
+            println!("Error: {}", error);
+        }
+    }else{
+        println!("Semantic analysis successful.");
+    }
+    
     let code_generator = CodeGenerator::new();
 
     code_generator.generate_code_from_program_ast(&mut ast)
