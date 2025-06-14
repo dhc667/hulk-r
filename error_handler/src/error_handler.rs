495cd57836ade4f8953acbbb3170e977257e8c69
CfM47 <jossuearteche000@gmail.com>
2025-06-14 01:47:54 -0400

    feat: 🎸 Added error handler

--- error_handler/src/error_handler.rs
+++ error_handler/src/error_handler.rs
@@ -1,13 +1,15 @@
-use crate::error::error::HulkError;
+use crate::error::error::{HulkError, HulkErrorTrait};
 
 pub struct ErrorHandler {
-    pub line_breaks: Vec<usize>,
+    program_text: String,
+    line_breaks: Vec<usize>,
     pub errors: Vec<HulkError>,
 }
 
 impl ErrorHandler {
     pub fn new(program_text: &str) -> Self {
         Self {
+            program_text: program_text.to_string(),
             line_breaks: Self::get_line_breaks(program_text),
             errors: Vec::new(),
         }
@@ -35,7 +37,46 @@ impl ErrorHandler {
         !self.errors.is_empty()
     }
 
-    pub fn get_error_messages(&self) -> Vec<String> {
-        self.errors.iter().map(|error| error.to_string()).collect()
+    pub fn get_error_messages(&mut self) -> Vec<String> {
+        self.errors
+            .sort_by(|a, b| a.get_position().cmp(&b.get_position()));
+
+        self.errors
+            .iter()
+            .map(|error| {
+                let line_number = self.get_line_number(error.get_position());
+                let line_start = self.line_breaks[line_number];
+                let line_end = if line_number + 1 < self.line_breaks.len() {
+                    self.line_breaks[line_number + 1]
+                } else {
+                    self.program_text.len()
+                };
+                let line_text = &self.program_text[line_start..line_end].trim_end_matches('\n');
+                let col = error.get_position() - line_start;
+                let pointer_line = format!("{}{}", " ".repeat(col), "^",);
+
+                format!(
+                    "{}\n --> line {}:{}\n  |\n{:3} | {}\n  |   {}\n",
+                    error.to_string(),
+                    line_number + 1,
+                    col + 1,
+                    line_number + 1,
+                    line_text,
+                    pointer_line
+                )
+            })
+            .collect()
+    }
+
+    fn get_line_number(&self, position: usize) -> usize {
+        match self.line_breaks.binary_search_by(|&line| {
+            if line > position {
+                std::cmp::Ordering::Greater
+            } else {
+                std::cmp::Ordering::Less
+            }
+        }) {
+            Ok(idx) | Err(idx) => idx,
+        }
     }
 }
