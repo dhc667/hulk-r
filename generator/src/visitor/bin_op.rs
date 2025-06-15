84fcc99c49326c61a67ecddb3804b636a0f461eb
francisco-prestamo <prestamo2003@icloud.com>
2025-06-15 15:36:49 -0400

    feat: enhance string operations

--- generator/src/visitor/bin_op.rs
+++ generator/src/visitor/bin_op.rs
@@ -12,6 +12,11 @@ impl GeneratorVisitor {
         rhs_result: VisitorResult,
         op: &BinaryOperator,
     ) -> VisitorResult {
+
+        if let At(_) | AtAt(_) = op {
+            return self.get_string_bin_op_visitor_result(op, lhs_result, rhs_result);
+        }
+
         if lhs_result.result_handle.is_none() {
             panic!("Expected a result handle for lhs of binary operator");
         }
@@ -170,31 +175,250 @@ impl GeneratorVisitor {
 
         let operation = match op {
              At(_) => {
-                // String concatenation implementation
-                // 1. Get string lengths for both operands
-                let len1_var = self.generate_tmp_variable();
-                let len2_var = self.generate_tmp_variable();
-                let total_len_var = self.generate_tmp_variable();
-                let total_len_plus_one_var = self.generate_tmp_variable();
+                // String concatenation implementation with type coercion
+                // Convert lhs operand to string if needed
+                let (lhs_str, lhs_conv) = match lhs_handle.handle_type {
+                    HandleType::Literal(LlvmType::String) | HandleType::Register(LlvmType::String) => {
+                        (lhs_handle.llvm_name.clone(), String::new())
+                    }
+                    HandleType::Literal(LlvmType::I1) | HandleType::Register(LlvmType::I1) => {
+                        let true_ptr = self.generate_tmp_variable();
+                        let false_ptr = self.generate_tmp_variable();
+                        let str_ptr = self.generate_tmp_variable();
+                        let code = format!(
+                            "{true_ptr} = getelementptr [6 x i8], [6 x i8]* @.true_str, i32 0, i32 0\n\
+                             {false_ptr} = getelementptr [7 x i8], [7 x i8]* @.false_str, i32 0, i32 0\n\
+                             {str_ptr} = select i1 {cond}, i8* {true_ptr}, i8* {false_ptr}\n",
+                            true_ptr = true_ptr,
+                            false_ptr = false_ptr,
+                            str_ptr = str_ptr,
+                            cond = lhs_handle.llvm_name
+                        );
+                        (str_ptr, code)
+                    }
+                    HandleType::Literal(LlvmType::F64) | HandleType::Register(LlvmType::F64) => {
+                        let num_buffer = self.generate_tmp_variable();
+                        let buffer_cast = self.generate_tmp_variable();
+                        let sn_ret = self.generate_tmp_variable();
+                        let str_len = self.generate_tmp_variable();
+                        let str_len_null = self.generate_tmp_variable();
+                        let result_buffer = self.generate_tmp_variable();
+                        let code = format!(
+                            "{num_buffer} = alloca [32 x i8], align 1\n\
+                             {buffer_cast} = bitcast [32 x i8]* {num_buffer} to i8*\n\
+                             {sn_ret} = call i32 (i8*, i8*, ...) @sprintf(i8* {buffer_cast}, i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.fstr2, i32 0, i32 0), double {val})\n\
+                             {str_len} = call i64 @strlen(i8* {buffer_cast})\n\
+                             {str_len_null} = add i64 {str_len}, 1\n\
+                             {result_buffer} = call i8* @malloc(i64 {str_len_null})\n\
+                             call void @strcpy(i8* {result_buffer}, i8* {buffer_cast})\n",
+                            num_buffer = num_buffer,
+                            buffer_cast = buffer_cast,
+                            sn_ret = sn_ret,
+                            str_len = str_len,
+                            str_len_null = str_len_null,
+                            result_buffer = result_buffer,
+                            val = lhs_handle.llvm_name
+                        );
+                        (result_buffer, code)
+                    }
+                    _ => panic!("Unsupported type for string concatenation"),
+                };
+                // Convert rhs operand to string if needed
+                let (rhs_str, rhs_conv) = match rhs_handle.handle_type {
+                    HandleType::Literal(LlvmType::String) | HandleType::Register(LlvmType::String) => {
+                        (rhs_handle.llvm_name.clone(), String::new())
+                    }
+                    HandleType::Literal(LlvmType::I1) | HandleType::Register(LlvmType::I1) => {
+                        let true_ptr = self.generate_tmp_variable();
+                        let false_ptr = self.generate_tmp_variable();
+                        let str_ptr = self.generate_tmp_variable();
+                        let code = format!(
+                            "{true_ptr} = getelementptr [6 x i8], [6 x i8]* @.true_str, i32 0, i32 0\n\
+                             {false_ptr} = getelementptr [7 x i8], [7 x i8]* @.false_str, i32 0, i32 0\n\
+                             {str_ptr} = select i1 {cond}, i8* {true_ptr}, i8* {false_ptr}\n",
+                            true_ptr = true_ptr,
+                            false_ptr = false_ptr,
+                            str_ptr = str_ptr,
+                            cond = rhs_handle.llvm_name
+                        );
+                        (str_ptr, code)
+                    }
+                    HandleType::Literal(LlvmType::F64) | HandleType::Register(LlvmType::F64) => {
+                        let num_buffer = self.generate_tmp_variable();
+                        let buffer_cast = self.generate_tmp_variable();
+                        let sn_ret = self.generate_tmp_variable();
+                        let str_len = self.generate_tmp_variable();
+                        let str_len_null = self.generate_tmp_variable();
+                        let result_buffer = self.generate_tmp_variable();
+                        let code = format!(
+                            "{num_buffer} = alloca [32 x i8], align 1\n\
+                             {buffer_cast} = bitcast [32 x i8]* {num_buffer} to i8*\n\
+                             {sn_ret} = call i32 (i8*, i8*, ...) @sprintf(i8* {buffer_cast}, i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.fstr2, i32 0, i32 0), double {val})\n\
+                             {str_len} = call i64 @strlen(i8* {buffer_cast})\n\
+                             {str_len_null} = add i64 {str_len}, 1\n\
+                             {result_buffer} = call i8* @malloc(i64 {str_len_null})\n\
+                             call void @strcpy(i8* {result_buffer}, i8* {buffer_cast})\n",
+                            num_buffer = num_buffer,
+                            buffer_cast = buffer_cast,
+                            sn_ret = sn_ret,
+                            str_len = str_len,
+                            str_len_null = str_len_null,
+                            result_buffer = result_buffer,
+                            val = rhs_handle.llvm_name
+                        );
+                        (result_buffer, code)
+                    }
+                    _ => panic!("Unsupported type for string concatenation"),
+                };
+                // Combine preambles for conversions
+                let len1 = self.generate_tmp_variable();
+                let len2 = self.generate_tmp_variable();
+                let total = self.generate_tmp_variable();
+                let total_plus_one = self.generate_tmp_variable();
                 let result_ptr = self.generate_tmp_variable();
-                
-                format!(
-                    // Get length of first string
+                lhs_conv + &rhs_conv + &format!(
+                    // Get lengths and allocate + concatenate as before
                     "{len1} = call i32 @strlen(i8* {lhs})\n\
-                    {len2} = call i32 @strlen(i8* {rhs})\n\
-                    {total_len} = add i32 {len1}, {len2}\n\
-                    {total_len_plus_one} = add i32 {total_len}, 1\n\
-                    {result_ptr} = call i8* @malloc(i32 {total_len_plus_one})\n\
-                    call i8* @strcpy(i8* {result_ptr}, i8* {lhs})\n\
-                    {result_handle} = call i8* @strcat(i8* {result_ptr}, i8* {rhs})\n",
-                    len1 = len1_var,
-                    len2 = len2_var,
-                    total_len = total_len_var,
-                    total_len_plus_one = total_len_plus_one_var,
+                     {len2} = call i32 @strlen(i8* {rhs})\n\
+                     {total} = add i32 {len1}, {len2}\n\
+                     {total_plus_one} = add i32 {total}, 1\n\
+                     {result_ptr} = call i8* @malloc(i32 {total_plus_one})\n\
+                     call i8* @strcpy(i8* {result_ptr}, i8* {lhs})\n\
+                     {res} = call i8* @strcat(i8* {result_ptr}, i8* {rhs})\n",
+                    len1 = len1,
+                    len2 = len2,
+                    total = total,
+                    total_plus_one = total_plus_one,
                     result_ptr = result_ptr,
-                    lhs = lhs_handle.llvm_name,
-                    rhs = rhs_handle.llvm_name,
-                    result_handle = result_handle
+                    lhs = lhs_str,
+                    rhs = rhs_str,
+                    res = result_handle
+                )
+            },
+
+            AtAt(_) => {
+                // String concatenation implementation with type coercion
+                // Convert lhs operand to string if needed
+                let (lhs_str, lhs_conv) = match lhs_handle.handle_type {
+                    HandleType::Literal(LlvmType::String) | HandleType::Register(LlvmType::String) => {
+                        (lhs_handle.llvm_name.clone(), String::new())
+                    }
+                    HandleType::Literal(LlvmType::I1) | HandleType::Register(LlvmType::I1) => {
+                        let true_ptr = self.generate_tmp_variable();
+                        let false_ptr = self.generate_tmp_variable();
+                        let str_ptr = self.generate_tmp_variable();
+                        let code = format!(
+                            "{true_ptr} = getelementptr [6 x i8], [6 x i8]* @.true_str, i32 0, i32 0\n\
+                             {false_ptr} = getelementptr [7 x i8], [7 x i8]* @.false_str, i32 0, i32 0\n\
+                             {str_ptr} = select i1 {cond}, i8* {true_ptr}, i8* {false_ptr}\n",
+                            true_ptr = true_ptr,
+                            false_ptr = false_ptr,
+                            str_ptr = str_ptr,
+                            cond = lhs_handle.llvm_name
+                        );
+                        (str_ptr, code)
+                    }
+                    HandleType::Literal(LlvmType::F64) | HandleType::Register(LlvmType::F64) => {
+                        let num_buffer = self.generate_tmp_variable();
+                        let buffer_cast = self.generate_tmp_variable();
+                        let sn_ret = self.generate_tmp_variable();
+                        let str_len = self.generate_tmp_variable();
+                        let str_len_null = self.generate_tmp_variable();
+                        let result_buffer = self.generate_tmp_variable();
+                        let code = format!(
+                            "{num_buffer} = alloca [32 x i8], align 1\n\
+                             {buffer_cast} = bitcast [32 x i8]* {num_buffer} to i8*\n\
+                             {sn_ret} = call i32 (i8*, i8*, ...) @sprintf(i8* {buffer_cast}, i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.fstr2, i32 0, i32 0), double {val})\n\
+                             {str_len} = call i64 @strlen(i8* {buffer_cast})\n\
+                             {str_len_null} = add i64 {str_len}, 1\n\
+                             {result_buffer} = call i8* @malloc(i64 {str_len_null})\n\
+                             call void @strcpy(i8* {result_buffer}, i8* {buffer_cast})\n",
+                            num_buffer = num_buffer,
+                            buffer_cast = buffer_cast,
+                            sn_ret = sn_ret,
+                            str_len = str_len,
+                            str_len_null = str_len_null,
+                            result_buffer = result_buffer,
+                            val = lhs_handle.llvm_name
+                        );
+                        (result_buffer, code)
+                    }
+                    _ => panic!("Unsupported type for string concatenation"),
+                };
+                // Convert rhs operand to string if needed
+                let (rhs_str, rhs_conv) = match rhs_handle.handle_type {
+                    HandleType::Literal(LlvmType::String) | HandleType::Register(LlvmType::String) => {
+                        (rhs_handle.llvm_name.clone(), String::new())
+                    }
+                    HandleType::Literal(LlvmType::I1) | HandleType::Register(LlvmType::I1) => {
+                        let true_ptr = self.generate_tmp_variable();
+                        let false_ptr = self.generate_tmp_variable();
+                        let str_ptr = self.generate_tmp_variable();
+                        let code = format!(
+                            "{true_ptr} = getelementptr [6 x i8], [6 x i8]* @.true_str, i32 0, i32 0\n\
+                             {false_ptr} = getelementptr [7 x i8], [7 x i8]* @.false_str, i32 0, i32 0\n\
+                             {str_ptr} = select i1 {cond}, i8* {true_ptr}, i8* {false_ptr}\n",
+                            true_ptr = true_ptr,
+                            false_ptr = false_ptr,
+                            str_ptr = str_ptr,
+                            cond = rhs_handle.llvm_name
+                        );
+                        (str_ptr, code)
+                    }
+                    HandleType::Literal(LlvmType::F64) | HandleType::Register(LlvmType::F64) => {
+                        let num_buffer = self.generate_tmp_variable();
+                        let buffer_cast = self.generate_tmp_variable();
+                        let sn_ret = self.generate_tmp_variable();
+                        let str_len = self.generate_tmp_variable();
+                        let str_len_null = self.generate_tmp_variable();
+                        let result_buffer = self.generate_tmp_variable();
+                        let code = format!(
+                            "{num_buffer} = alloca [32 x i8], align 1\n\
+                             {buffer_cast} = bitcast [32 x i8]* {num_buffer} to i8*\n\
+                             {sn_ret} = call i32 (i8*, i8*, ...) @sprintf(i8* {buffer_cast}, i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.fstr2, i32 0, i32 0), double {val})\n\
+                             {str_len} = call i64 @strlen(i8* {buffer_cast})\n\
+                             {str_len_null} = add i64 {str_len}, 1\n\
+                             {result_buffer} = call i8* @malloc(i64 {str_len_null})\n\
+                             call void @strcpy(i8* {result_buffer}, i8* {buffer_cast})\n",
+                            num_buffer = num_buffer,
+                            buffer_cast = buffer_cast,
+                            sn_ret = sn_ret,
+                            str_len = str_len,
+                            str_len_null = str_len_null,
+                            result_buffer = result_buffer,
+                            val = rhs_handle.llvm_name
+                        );
+                        (result_buffer, code)
+                    }
+                    _ => panic!("Unsupported type for string concatenation"),
+                };
+                // Combine preambles for conversions
+                let len1 = self.generate_tmp_variable();
+                let len2 = self.generate_tmp_variable();
+                let total = self.generate_tmp_variable();
+                let total_plus_one = self.generate_tmp_variable();
+                let result_ptr = self.generate_tmp_variable();
+                let space_ptr = self.generate_tmp_variable();
+                lhs_conv + &rhs_conv + &format!(
+                    // Get lengths and allocate + concatenate with space in the middle
+                    "{len1} = call i32 @strlen(i8* {lhs})\n\
+                     {len2} = call i32 @strlen(i8* {rhs})\n\
+                     {total} = add i32 {len1}, {len2}\n\
+                     {total_plus_one} = add i32 {total}, 1\n\
+                     {result_ptr} = call i8* @malloc(i32 {total_plus_one})\n\
+                     call i8* @strcpy(i8* {result_ptr}, i8* {lhs})\n\
+                     {space_ptr} = getelementptr [2 x i8], [2 x i8]* @.space_str, i32 0, i32 0\n\
+                     call i8* @strcat(i8* {result_ptr}, i8* {space_ptr})\n\
+                     {res} = call i8* @strcat(i8* {result_ptr}, i8* {rhs})\n",
+                    len1 = len1,
+                    len2 = len2,
+                    total = total,
+                    total_plus_one = total_plus_one,
+                    result_ptr = result_ptr,
+                    lhs = lhs_str,
+                    space_ptr = space_ptr,
+                    res = result_handle,
+                    rhs = rhs_str
                 )
             },
             
