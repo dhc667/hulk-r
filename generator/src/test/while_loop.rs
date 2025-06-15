2a9540772b5827d883fe09f4b9e5356cfa48312b
francisco-prestamo <prestamo2003@icloud.com>
2025-06-14 23:08:04 -0400

    feat: enhance for loop handling and add while loop test cases

--- generator/src/test/while_loop.rs
+++ generator/src/test/while_loop.rs
@@ -1,4 +1,4 @@
-use crate::test::lli_interface::lli_f64;
+use crate::test::lli_interface::{lli_f64, lli_string};
 
 use super::generate_code;
 
@@ -16,3 +16,39 @@ fn simple_while() {
     println!("{}", llvm);
     assert_eq!(lli_f64(&llvm).unwrap(), 1.0);
 }
+
+// #[test]
+// fn simple_for() {
+//     let llvm = generate_code(
+//         "
+//         for (x in [1,2,3]) {
+//             print(x);
+//         };
+        
+//         ",
+//     );
+
+//     println!("{}", llvm);
+//     assert_eq!(lli_f64(&llvm).unwrap(), 1.0);
+// }
+
+#[test]
+fn simple_while_2() {
+    let llvm = generate_code(
+        "
+        let i = 0 , array = [1,2,3,4,5,6,7] in {
+            while(i < 3) {
+                print(array[i]);
+                i := i + 1;
+            };
+
+        };",
+    );
+
+    println!("{}", llvm);
+    assert_eq!(lli_string(&llvm).unwrap(), "1.000000
+2.000000
+3.000000");
+}
+
+
