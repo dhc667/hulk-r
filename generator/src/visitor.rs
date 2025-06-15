ddd859cd70436ca974032bc4978f10fe86cb5bfa
francisco-prestamo <prestamo2003@icloud.com>
2025-06-15 10:04:28 -0400

    feat: implement for loop handling with LLVM IR code generation

--- generator/src/visitor.rs
+++ generator/src/visitor.rs
@@ -342,8 +342,122 @@ impl ExpressionVisitor<VisitorResult> for GeneratorVisitor {
     fn visit_for(&mut self, node: &mut ast::For) -> VisitorResult {
         let iterable_result = node.iterable.accept(self);
         let element_id = node.element.id.clone();
-        self.handle_for(&element_id, iterable_result, node)
+        let body_result = node.body.accept(self);
+
+
+        let iterable_result_handle = iterable_result
+            .result_handle
+            .expect("Expected a result handle for iterable expression of for loop");
+
+        let index_var = self.generate_tmp_variable();
+        let init_code = format!("  {} = alloca i64\n  store i64 0, i64* {}\n", index_var, index_var);
+
+        let length_ptr = self.generate_tmp_variable();
+        let length_var = self.generate_tmp_variable();
+
+        let llvm_elem_type = "i64";
+
+        let length_ptr_code = format!("  {} = getelementptr inbounds {}, {}* {}, i64 0\n",
+                                      length_ptr, llvm_elem_type, llvm_elem_type,
+                                      iterable_result_handle.llvm_name);
+
+        let length_code = format!("  {} = load {}, {}* {}\n",
+                                  length_var, llvm_elem_type, llvm_elem_type, length_ptr);
+
+        let condition_name = self.generate_tmp_variable();
+
+        let condition_code = format!("{cond} = icmp slt i32 %{index}, %{length}",
+                                     cond = condition_name,
+                                     index = index_var,
+                                     length = length_var
+        );
+
+        
+        
+        let (loop_label, body_label, loop_exit_label) = self.generate_loop_labels();
+
+        let loop_setup = init_code.to_string()
+                + length_ptr_code.as_str()
+                + length_code.as_str()
+                + self.branch_jump_statement(&loop_label).as_str()
+                + &self.block_start(&loop_label)
+                + condition_code.as_str()
+                + &self.branch_choice_statement(
+                &condition_name,
+                &body_label,
+                &loop_exit_label,
+        );
+
+
+        let body_code = self.block_start(&body_label)
+            + &body_result.preamble
+            + &self.branch_jump_statement(&loop_label);
+    
+        let exit_code = self.block_start(&loop_exit_label);
+    
+        let preamble = loop_setup + &body_code + &exit_code;
+    
+        VisitorResult {
+            preamble,
+            result_handle: None,
+        }
     }
+    //
+    // pub(crate) fn handle_while(
+    //     &mut self,
+    //     condition_result: VisitorResult,
+    //     body_result: VisitorResult,
+    // ) -> VisitorResult {
+    //     let condition_result_handle = condition_result
+    //         .result_handle
+    //         .expect("Expected a result handle for condition of while statement");
+    //
+    //     let (loop_label, body_label, loop_exit_label) = self.generate_loop_labels();
+    //
+    //     // here we assume the type of the handle returned by the condition is i1, SA is
+    //     // responsible for this
+    //     let loop_setup = self.branch_jump_statement(&loop_label)
+    //         + &self.block_start(&loop_label)
+    //         + &condition_result.preamble
+    //         + &self.branch_choice_statement(
+    //         &condition_result_handle.llvm_name,
+    //         &body_label,
+    //         &loop_exit_label,
+    //     );
+    //
+    //     let body_code = self.block_start(&body_label)
+    //         + &body_result.preamble
+    //         + &self.branch_jump_statement(&loop_label);
+    //
+    //     let exit_code = self.block_start(&loop_exit_label);
+    //
+    //     let preamble = loop_setup + &body_code + &exit_code;
+    //
+    //     VisitorResult {
+    //         preamble,
+    //         result_handle: None,
+    //     }
+    // }
+    //
+    // /// # Description
+    // ///
+    // /// Uses the same global tmp_variable id to create globally unique loop, body,
+    // /// loop_exit labels
+    // ///
+    // /// # Examples
+    // ///
+    // /// - If we generate a temporary variable %.0, and then generate these labels, we'll get
+    // /// loop.1, body.1, loop_exit.1
+    // /// - If we generate the labels first, we'll get loop.0, body.0, loop_exit.0
+    // pub(crate) fn generate_loop_labels(&mut self) -> (String, String, String) {
+    //     let l = format!("loop.{}", self.tmp_variable_id);
+    //     let b = format!("body.{}", self.tmp_variable_id);
+    //     let le = format!("loop_exit.{}", self.tmp_variable_id);
+    //
+    //     self.tmp_variable_id += 1;
+    //
+    //     (l, b, le)
+    // }
 
     fn visit_un_op(&mut self, node: &mut ast::UnOp) -> VisitorResult {
         let inner_result = node.rhs.accept(self);
